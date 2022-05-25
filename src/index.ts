e8387ae16df36f7a8018e2a8fb8c9804cba718b8
Benjoow <benjamin.wojeez@gmail.com>
2022-05-25 14:05:54 +0200

    delete absolute position for more flexibility

--- src/index.ts
+++ src/index.ts
@@ -6,63 +6,128 @@ const cardsData = async ():Promise<Object> => {
   return response.json();
 }
 
-const createCard = async ():Promise<void> => {
+const createCards = async (cardsInfo: Array<Object>):Promise<void> => {
   const main: HTMLElement = document.querySelector("main")!;
-  const data: Array<Object> = await cardsData();
-  console.log(await data);
-  data.forEach(item => {
+  cardsInfo.forEach(item => {
     const card: HTMLDivElement = document.createElement("div");
-    card.classList.add("container", "secondary-card");
-    const illustrationCard: HTMLDivElement = document.createElement("div");
-    illustrationCard.classList.add("illustration__card");
+    card.classList.add("secondary-container", "secondary-card");
+    // const secondaryBottom: HTMLDivElement = document.createElement("div");
+    // secondaryBottom.classList.add("illustration__card");
     switch (item.title) {
       case 'Work' :
-        illustrationCard.setAttribute("id", "work");
+        card.setAttribute("id", "work");
         break;
       case 'Play' :
-        illustrationCard.setAttribute("id", "play");
+        card.setAttribute("id", "play");
         break;
       case 'Study' :
-        illustrationCard.setAttribute("id", "study");
+        card.setAttribute("id", "study");
         break;
       case 'Exercise' :
-        illustrationCard.setAttribute("id", "exercise");
+        card.setAttribute("id", "exercise");
         break;
       case 'Social' :
-        illustrationCard.setAttribute("id", "social");
+        card.setAttribute("id", "social");
         break;
       case 'Self Care' :
-        illustrationCard.setAttribute("id", "selfCare");
+        card.setAttribute("id", "selfCare");
         break;
       default:
         console.log("Data Error");
       }
+    const secondaryBottom = document.createElement("div");
+    secondaryBottom.classList.add("secondary-card__bottom");
     const contentTop = document.createElement("div");
     contentTop.classList.add("content__top");
     contentTop.appendChild(document.createElement("h2")).textContent = item.title;
     const buttonOption = document.createElement("img");
     buttonOption.src = "./src/images/icon-ellipsis.svg";
     buttonOption.setAttribute('alt', 'options button');
+    buttonOption.classList.add("svgColor");
     contentTop.appendChild(buttonOption);
     const contentBottom = document.createElement("div");
     contentBottom.classList.add("content__bottom");
     const hours = document.createElement("p");
     const previous = document.createElement("p");
-    hours.textContent = `${item.timeframes.weekly.current} hrs`;
-    previous.textContent = `Last Week - ${item.timeframes.weekly.previous} hrs`;
+    hours.textContent = `${item.current}hrs`;
+    previous.textContent = `Last Week - ${item.previous}hrs`;
     contentBottom.appendChild(hours);
-    contentBottom.appendChild(previous);
+    contentBottom.appendChild(previous);    
 
-    console.log(item);
     main.appendChild(card);
-    card.appendChild(illustrationCard);
-    card.appendChild(contentTop);
-    card.appendChild(contentBottom);
+    card.appendChild(secondaryBottom);
+    secondaryBottom.appendChild(contentTop);
+    secondaryBottom.appendChild(contentBottom);
+  })
+}
+
+const deleteCards = () => {
+  const secondaryCards = document.querySelectorAll(".secondary-card");
+  secondaryCards.forEach(item => {
+    item.remove();
+  })
+}
+
+const activeMenu = (event: Event) => {
+  let activeElement = event.target;
+  const menu: NodeListOf<HTMLLIElement> = document.querySelectorAll("#main-card__content-bottom li");
+  menu.forEach(item => {
+    if(activeElement === item) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  })
+}
+
+const sortByTimePeriod = async (period: string): Promise<Array<Object>> => {
+  const data: Array<Object> = await cardsData();  
+  const dataSort: { title: string; current: number; previous: number} [] = [];
+  data.forEach(item => {
+    dataSort.push({ title: item.title,
+                    current: item.timeframes[period].current,
+                    previous: item.timeframes[period].previous
+                 })  
+  })
+  return dataSort;
+}
+
+const selectMenu = () => {
+  const weeklyButton: HTMLLIElement = document.querySelector('#weeklyButton')!;
+  const dailyButton: HTMLLIElement = document.querySelector('#dailyButton')!;
+  const monthlyButton: HTMLLIElement = document.querySelector('#monthlyButton')!;
+
+  
+  weeklyButton.addEventListener('click', async (event)=> {
+    activeMenu(event)
+    deleteCards();
+    const data =  await sortByTimePeriod("weekly");
+    createCards(data);
+  })
+  dailyButton.addEventListener('click', async (event) => {
+    activeMenu(event)
+    deleteCards();
+    const data = await sortByTimePeriod("daily");
+    createCards(data);
+  })
+  monthlyButton.addEventListener('click', async (event) => {
+    activeMenu(event)
+    deleteCards();
+    const data = await sortByTimePeriod("monthly");
+    createCards(data);
   })
 }
 
 
-createCard();
+const initPage = async () => {
+  const weeklyButton: HTMLLIElement = document.querySelector('#weeklyButton')!;
+  weeklyButton.classList.add("active");
+  const data = await sortByTimePeriod("weekly");
+  createCards(data);
+  selectMenu();
+}
+
+initPage();
 
 
 export {};
